5c2cf805e39af9120507d8db288df78e0de7f451
Jayesh <jayeshajay2003@gmail.com>
2026-03-08 16:05:58 +0530

    Pascal's Triangle

---
+++ Patterens/PascalsTriangle.java
@@ -0,0 +1,16 @@
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        
+        for(int i=0;i<=4;i++){
+            int num=1;
+            for(int j=0;j<=4-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print(num+" ");
+                num = num*(i-j)/(j+1);
+            }
+            System.out.println();
+        }
+    }
+}
