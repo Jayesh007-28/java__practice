71e4ca440fe090a1ddddccf60cb99c8009f7b44d
Jayesh <jayeshajay2003@gmail.com>
2026-03-13 14:42:10 +0530

    Reverse the given array

---
+++ Arrays/ReversArray.java
@@ -0,0 +1,12 @@
+public class ReversArray {
+
+    static void reverseArray(int arr[]){
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int [] arr = {10,20,30,40,50};
+        reverseArray(arr);
+    }
+}
