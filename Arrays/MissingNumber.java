f056fd6516250e51f9bfc400b5c0f20de6ac9910
Jayesh <jayeshajay2003@gmail.com>
2026-03-23 14:53:05 +0530

    Missing number

---
+++ Arrays/MissingNumber.java
@@ -0,0 +1,14 @@
+public class MissingNumber {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,5};
+        int n = arr.length+1;
+
+        int expectedSum = n*(n+1)/2;
+        int actualSum=0;
+
+        for(int i=0;i<arr.length;i++){
+            actualSum+=arr[i];
+        }
+        System.out.println("Missing Number : "+(expectedSum-actualSum));
+    }
+}
